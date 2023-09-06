df05d398057dedefbb5edd56363d85eefd84bdb9
Yusuf Akyüz <yusufakyus47@gmail.com>
2023-09-06 16:12:13 +0300

    Layout items defined

--- app/src/main/java/com/example/catchtherick/MainActivity.java
+++ app/src/main/java/com/example/catchtherick/MainActivity.java
@@ -3,8 +3,28 @@ package com.example.catchtherick;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView timeText;
+    TextView scoreText;
+    int number;
+    ImageView imageView1;
+    ImageView imageView2;
+    ImageView imageView3;
+    ImageView imageView4;
+    ImageView imageView5;
+    ImageView imageView6;
+    ImageView imageView7;
+    ImageView imageView8;
+    ImageView imageView9;
+
+    ImageView [] imageArray;
+    Handler handler;
+    Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
