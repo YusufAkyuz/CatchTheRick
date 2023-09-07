7b16f47574980b0c0f8390903b872ccbd5600a8a
Yusuf Akyüz <yusufakyus47@gmail.com>
2023-09-07 10:28:24 +0300

    Acction Bar added

--- app/src/main/java/com/example/catchtherick/MainActivity.java
+++ app/src/main/java/com/example/catchtherick/MainActivity.java
@@ -123,7 +123,7 @@ public class MainActivity extends AppCompatActivity {
                 int i = random.nextInt(9);
                 imageArray[i].setVisibility(View.VISIBLE);
                 handler.postDelayed(this, 500);
-                
+
             }
         };
         handler.post(runnable);
